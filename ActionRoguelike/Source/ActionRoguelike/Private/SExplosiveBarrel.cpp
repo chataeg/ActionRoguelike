612cafa241deadf34ee01665b157107fa5f3c019
Tae Gwan Cha <chataeg@gmail.com>
2025-05-11 17:51:49 +0900

    17 : barrel 숙제 강의 견본에 맞게 수정

--- ActionRoguelike/Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
+++ ActionRoguelike/Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -13,12 +13,25 @@ ASExplosiveBarrel::ASExplosiveBarrel()
 	PrimaryActorTick.bCanEverTick = true;
 
 	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	StaticMesh->SetSimulatePhysics(true);
 	RootComponent = StaticMesh;
 	
 	//StaticMesh->
 	RadialForce = CreateDefaultSubobject<URadialForceComponent>("RadialForce");
 	RadialForce->SetupAttachment(RootComponent);
 
+	RadialForce->SetAutoActivate(false);
+
+	RadialForce->Radius = 750.f;
+	RadialForce->ImpulseStrength = 2500.f;
+	// 다른 오브젝트의 원래 속도를 무시함 (질량을 무시)
+	RadialForce->bImpulseVelChange = true;
+
+	// WorldDynamic 에도 영향을 줄 수 있게 변경
+	RadialForce->AddCollisionChannelToAffect(ECC_WorldDynamic);
+	
+
+	
 	
 }
 
@@ -27,13 +40,23 @@ void ASExplosiveBarrel::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	
+}
+
+void ASExplosiveBarrel::PostInitializeComponents()
+{
+	// BeginPlay 이전에 호출된다.
+	// 무조건 부모 호출
+	Super::PostInitializeComponents();
+
 	// 원래 존재하는 DELEGATE 에 바인딩
+	// Beginplay 에 넣어도 되지만 hotreload 시 안 될 수 있음 (beginplay 전에 construct 되기 때문) 
 	StaticMesh->OnComponentHit.AddDynamic(this,&ASExplosiveBarrel::OnHit);
 	
 }
 
 void ASExplosiveBarrel::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
-	FVector NormalImpulse, const FHitResult& Hit)
+                              FVector NormalImpulse, const FHitResult& Hit)
 {
 	
 	UE_LOG(LogTemp,Log,TEXT("ASExplosiveBarrel::OnHit"));
